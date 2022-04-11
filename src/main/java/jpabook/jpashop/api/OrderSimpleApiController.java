7978f556e76c8663b19fe3289fe64095e8863f4a
imcool2551 <imcool2551@gmail.com>
2022-04-11 19:18:50 +0900

    xToOne: v2.dto 노출

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * ManyToOne, OneToOne: 단방향 연관관계
@@ -28,11 +33,45 @@ public class OrderSimpleApiController {
      */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
-        List<Order> result = orderRepository.findAllByString(new OrderSearch());
-        for (Order order : result) {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : orders) {
             order.getMember().getName(); // 프록시 강제 초기화
             order.getDelivery().getAddress(); // 프록시 강제 초기화
         }
-        return result;
+        return orders;
     }
+
+    /**
+     * V2. DTO 변환
+     * - 프록시 초기화로 인한 N+1 문제 발생
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // 쿼리 1번 -> (ORDER 2개) -> Member 2개 쿼리 -> Delivery 2개 쿼리 -> 5번 쿼리
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
 }
